c26a0cb5344ea2f983e7f98a7d83f1db0192b95e
onurhanas <113357599+onurhanas@users.noreply.github.com>
2022-10-02 23:30:18 +0300

    while

---
+++ While/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   Console.WriteLine("Bir sayı giriniz");
+            int sayi = int.Parse(Console.ReadLine());
+        int sayac=1;
+        int toplam =0;
+          while(sayac<=sayi){
+            toplam+=sayac;
+            sayac++;
+
+          }
+          Console.WriteLine(toplam/sayi);
+            string[] arabalar = {"BMW","Ford","Toyota","Nissan"};
+        foreach (var araba in arabalar)
+        {
+            Console.WriteLine(araba);
+        }
+        }
+      
+    }
+}
